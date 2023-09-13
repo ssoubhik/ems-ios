80003db4246b2f324d026e0e5807f8d6723ad0fc
Soubhik Sarkhel <soubhik@annantasourcellc.onmicrosoft.com>
2023-09-13 17:43:35 +0530

    feat: initial commit

---
+++ EMS/EMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMSApp.swift
+//  EMS
+//
+//  Created by Soubhik Sarkhel on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct EMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
